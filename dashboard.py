774e7e2427e1f8934db2c95a8ef542d78de249ac
hayesall <alexander@batflyer.net>
2022-03-16 16:07:58 -0400

    ✨ Add base page rework, dashboard routes

--- dashboard.py
+++ dashboard.py
@@ -1,38 +1,103 @@
+# Copyright 2022 Alexander L. Hayes
+# MIT License
+
 import argparse
 import json
-import time
+
 from flask import Flask, render_template, request
 import plotly
-import plotly.graph_objs as go
+import plotly.graph_objects as go
+from plotly import subplots
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
+METADATA = pd.read_csv("user_metadata.csv")
 
-SLEEP_DELAY = 0
 DEFAULT_USER = "demo-user-1"
 DEFAULT_VIEW = "sleep"
 
 
+def get_users():
+    """Get pairs of full user id's and user slugs."""
+
+    # TODO(hayesall): @cache?
+
+    return METADATA[["user_full", "user"]].to_numpy().tolist()
+
+
 @app.route("/")
 def index():
-    time.sleep(SLEEP_DELAY)
     data = load_user_from_csv(DEFAULT_USER, DEFAULT_VIEW)
-    return render_template("base.html", plot=data)
+    return render_template(
+        "pages/user_overview.html",
+        page_title="User Overview",
+        users=get_users(),
+        plot=data,
+    )
+
+@app.route("/compare-users/")
+def compare_users():
+    return render_template(
+        "pages/compare_users.html",
+        page_title="Compare Users",
+        plot="null",
+    )
+
+@app.route("/summary-statistics/")
+def data_summary_statistics():
+    return render_template(
+        "pages/data_summary_statistics.html",
+        page_title="Data Summary Statistics",
+        user_dataframe=METADATA.to_html(
+            table_id="summaryTable",
+            justify="left",
+            columns=["user", "blood_oxygenation", "heart_rate", "sleep", "stress"],
+            classes=["table", "table-striped", "table-hover"],
+        ),
+        plot=None,
+    )
 
 @app.route("/sleep-bpm/")
 def sleep_bpm():
-    time.sleep(SLEEP_DELAY)
-    data = load_user_from_csv(DEFAULT_USER, DEFAULT_VIEW)
-    return render_template("base.html", plot=data)
+    data = load_user_sleep_bpm(DEFAULT_USER)
+    return render_template(
+        "pages/sleep_bpm.html",
+        page_title="Sleep + BPM",
+        users=get_users(),
+        plot=data,
+    )
 
+def load_user_sleep_bpm(user):
+    user_sleep = pd.read_csv(f"data/sleep/{user}.csv")
+    user_bpm = pd.read_csv(f"data/heart_rate/{user}.csv")
+
+    fig = subplots.make_subplots(rows=2, shared_xaxes=True)
+    fig.add_trace(go.Scatter(x=user_sleep.timestamp, y=user_sleep.level, line_shape="hv"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=user_bpm.timestamp, y=user_bpm.bpm), row=2, col=1)
+
+    return json.dumps(
+        fig,
+        cls=plotly.utils.PlotlyJSONEncoder,
+    )
+
+@app.route("/sleep-spo2/")
+def sleep_spo2():
+    return render_template("pages/sleep_spo2.html", page_title="Sleep + SpO2", plot="null")
+
+@app.route("/bpm-stress/")
+def sleep_bpm_stress():
+    return render_template("pages/bpm_stress.html", page_title="BPM + Stress", plot=None)
+
+@app.route("/licenses/")
+def licenses():
+    return render_template("pages/licenses.html", page_title="Open Source Licenses", plot=None)
 
 @app.route("/update_view", methods=["GET", "POST"])
 def update_view():
     user = request.args["user"]
     view = request.args["view"]
-
-    time.sleep(SLEEP_DELAY)
     graphJSON = load_user_from_csv(user, view)
     return graphJSON
 
