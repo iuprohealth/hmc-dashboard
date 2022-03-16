9c5c6d45fd6ea555694b91ebaab45350e86db59a
hayesall <alexander@batflyer.net>
2022-03-16 16:05:39 -0400

    ✨ Add script for building a user metadata table

---
+++ build_user_table.py
@@ -0,0 +1,44 @@
+# Copyright 2022 Alexander L. Hayes
+
+"""
+The 'data/' directory contains slow-changing information, so it's
+convenient to generate some metadata about the files in it.
+"""
+
+from collections import defaultdict
+import os
+from pathlib import Path
+
+import pandas as pd
+
+
+def slug_string(input_string):
+    if len(input_string) > 15:
+        return input_string[:7]
+    return input_string
+
+
+if __name__ == "__main__":
+
+    # The "data_types" are defined as the set of folders in the "data" directory.
+    data_types = sorted(os.listdir("data"))
+
+    users = defaultdict(lambda: [0] * len(data_types))
+
+    for dir in os.listdir("data"):
+
+        path = Path("data").joinpath(dir)
+
+        for file in os.listdir(path):
+            with open(path.joinpath(file), "r") as fh:
+                lines = len(fh.read().splitlines())
+
+            users[file.replace(".csv", "")][data_types.index(dir)] = lines
+
+    df = pd.DataFrame(
+        [(user, slug_string(user), *users[user]) for user in users],
+        columns=['user_full', 'user'] + data_types,
+    )
+
+    print("Writing to user_metadata.csv")
+    df.to_csv("user_metadata.csv", index=None)
